df61e1c1c4c416037fac4f8d90c421e44a98f7b9
pazgalov2georgia-debug <pazgalov2georgia@gmail.com>
2026-01-11 08:32:12 -0800

    Update Program.cs
    GeorgianFlagCompleted

--- Program.cs
+++ Program.cs
@@ -1,36 +1,82 @@
-﻿using System.Dynamic;
-
 Console.Write("Please, specify the number of floors: ");
-int flrs = int.Parse(Console.ReadLine());
+int totalFloors = int.Parse(Console.ReadLine());
 Console.Write("Please, specify the number of bulding entrances: ");
-int entrs = int.Parse(Console.ReadLine());
+int totalEntrances = int.Parse(Console.ReadLine());
 Console.Write("Please, specify the number of apatments per floor: ");
-int apts = int.Parse(Console.ReadLine());
+int roomsPerFloor = int.Parse(Console.ReadLine());
 
-int width = apts * entrs;
 
-if (flrs < 11)
+const int CellWidth = 4;
+
+for (int floor = totalFloors - 1; floor >= 0; floor--)
 {
-    Console.WriteLine("Bulding must be at least 11 floors to make a Georgian flag");
-    return;
-}
-
-if (width < 11)
-{
-    Console.WriteLine("Product of entrances and apartments per floor must be at least 11 to make a Georgian flag");
-    return;
-}
-
-for (int e = 1; e <= entrs; e++)
-{
-    for (int f = 1; f <= flrs + 1; f++)
+    for (int entrance = 0; entrance < totalEntrances; entrance++)
     {
-        for (int fpf = 1; fpf <= apts; fpf++)
+        for (int roomOnFloorIndex = 0; roomOnFloorIndex < roomsPerFloor; roomOnFloorIndex++)
         {
-            
-        }
-        
-    }
-    
-}  
+            var WindowInd = (roomOnFloorIndex + 1) + floor * roomsPerFloor + entrance * roomsPerFloor * totalFloors;
 
+            Console.ForegroundColor = GetRoomColor(roomOnFloorIndex, floor, entrance, totalFloors, totalEntrances, roomsPerFloor);
+            //Console.Write($"[{WindowInd,CellWidth}]");
+            Console.Write($"[{WindowInd:D3}]");
+            // Thread.Sleep(1000);
+        }
+        // Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+
+static ConsoleColor GetRoomColor(int roomOnFloorIndex, int floor, int entrance, int totalFloors, int totalEntrances, int roomsPerFloor)
+{
+    int bldWidth = roomsPerFloor * totalEntrances;
+    int bldHight = totalFloors;
+
+    int roomX = roomOnFloorIndex + entrance * roomsPerFloor;
+    int roomY = floor;
+    int midX = bldWidth / 2;
+    int midY = bldHight / 2;
+    int smallCrossXLeft = midX / 2;
+    int smallCrossXRight = midX + (bldWidth - midX) / 2;
+    int smallCrossYUp = midY + (bldHight - midY) / 2;
+    int smallCrossYDown = midY / 2;
+
+
+
+    if (roomX == bldWidth / 2 || roomY == bldHight / 2) //big cross
+    {
+        return ConsoleColor.Red;
+    }
+    //Define Angles Apts for all Crosses
+    bool is1CrossCorners = roomX == smallCrossXLeft - 1 && roomY == smallCrossYDown - 1 || roomX == smallCrossXLeft + 1 && roomY == smallCrossYDown + 1 || roomX == smallCrossXLeft + 1 && roomY == smallCrossYDown - 1 || roomX == smallCrossXLeft - 1 && roomY == smallCrossYDown + 1;
+    bool is2CrossCorners = roomX == smallCrossXLeft - 1 && roomY == smallCrossYUp - 1 || roomX == smallCrossXLeft + 1 && roomY == smallCrossYUp + 1 || roomX == smallCrossXLeft + 1 && roomY == smallCrossYUp - 1 || roomX == smallCrossXLeft - 1 && roomY == smallCrossYUp + 1;
+    bool is3CrossCorners = roomX == smallCrossXRight - 1 && roomY == smallCrossYUp - 1 || roomX == smallCrossXRight + 1 && roomY == smallCrossYUp + 1 || roomX == smallCrossXRight + 1 && roomY == smallCrossYUp - 1 || roomX == smallCrossXRight - 1 && roomY == smallCrossYUp + 1;
+    bool is4CrossCorners = roomX == smallCrossXRight - 1 && roomY == smallCrossYDown - 1 || roomX == smallCrossXRight + 1 && roomY == smallCrossYDown + 1 || roomX == smallCrossXRight + 1 && roomY == smallCrossYDown - 1 || roomX == smallCrossXRight - 1 && roomY == smallCrossYDown + 1;
+    //Remove Angles for all Crosses
+    if (is1CrossCorners || is2CrossCorners || is3CrossCorners || is4CrossCorners)
+    {
+        return ConsoleColor.White;
+    }
+
+    if (roomX >= smallCrossXLeft - 1 && roomX <= smallCrossXLeft + 1 && roomY >= smallCrossYDown - 1 && roomY <= smallCrossYDown + 1) //small cross #1
+    {
+        return ConsoleColor.Red;
+    }
+
+
+    if (roomX >= smallCrossXLeft - 1 && roomX <= smallCrossXLeft + 1 && roomY >= smallCrossYUp - 1 && roomY <= smallCrossYUp + 1) //small cross #2
+    {
+        return ConsoleColor.Red;
+    }
+
+    if (roomX >= smallCrossXRight - 1 && roomX <= smallCrossXRight + 1 && roomY >= smallCrossYUp - 1 && roomY <= smallCrossYUp + 1) //small cross #3
+    {
+        return ConsoleColor.Red;
+    }
+
+    if (roomX >= smallCrossXRight - 1 && roomX <= smallCrossXRight + 1 && roomY >= smallCrossYDown - 1 && roomY <= smallCrossYDown + 1) //small cross #4
+    {
+        return ConsoleColor.Red;
+    }
+    return ConsoleColor.Gray;
+
+}
